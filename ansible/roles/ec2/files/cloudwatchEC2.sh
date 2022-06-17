2829f1032ed2982ea07c18da1b75de3b725aba1f
Michael Gibney <michaelgibney1@gmail.com>
2022-06-16 17:09:24 -0700

    updated handlers and scripts

--- ansible/roles/ec2/files/cloudwatchEC2.sh
+++ ansible/roles/ec2/files/cloudwatchEC2.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# CREATE IAM USER / ROLE FOR CLOUDWATCH PERMISSIONS FIRST
+
+# CHECK LINUX VERSION ON EC2 INSTANCE
+export EC2_VERSION=$(grep -E -w 'VERSION|NAME|PRETTY_NAME' /etc/os-release)
+
+# INSTALL AGENT
+sudo yum install -y awslogs
+
+# VIEW LOG CONFIGS
+cat /etc/awslogs/awslogs.conf
+
+# EDIT DEFAULT REGION FOR AGGREGATING LOGS
+sudo nano /etc/awslogs/awscli.conf
